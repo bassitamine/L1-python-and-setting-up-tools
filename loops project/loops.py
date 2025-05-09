4532eea2b58376a6ea2d153d67872e10104e7cf6
bassitamine <Bassitamine76@gmail.com>
2025-05-09 16:48:10 +0200

    loops

---
+++ loops project/loops.py
@@ -0,0 +1,10 @@
+game = "Tekken 7"
+for y in game:
+    print(y)
+for y in range(0,500,50):
+    print(y)
+adj=["red","healthy","tasty"]
+fruits=["oranges","banana","cherry"]
+for a in adj:
+    for b in fruits:
+       print(a,b)
